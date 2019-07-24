00feee420992e96191a162b9d230a75d52181442
Aditya Pratap <adityapratap7344@gmail.com>
2019-07-25 02:09:37 +0530

    added redux and implemented user reducer and user action to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch,Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -9,32 +10,37 @@ import Header from './ui/ui-components/header/header.component.jsx';
 import SignInAndSignUpPage from './ui/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component.jsx';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
- 
+
+import { setCurrentUser } from './redux/user/user.actions';
 
 class  App extends React.Component {
 
-	constructor(){
+	//Contructor not needed anymore (using Redux now)
+	/*constructor(){
 		super();
 
 		this.state = {
 			currentUser : null,
 		}
-	}
+	}*/
 
 	unsubscribeFromAuth = null
 
 	componentDidMount() {
+
+		const { setCurrentUser } = this.props;
+
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			 
 			if(userAuth){
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.onSnapshot(snapShot => {
-					this.setState({
-						currentUser: {
+
+					setCurrentUser({
 							id:snapShot.id,
 							...snapShot.data()
-						}
+						
 					});
 
 				} );
@@ -42,7 +48,7 @@ class  App extends React.Component {
 				
 			}
 			else{
-				this.setState({ currentUser: userAuth});
+				setCurrentUser(userAuth);
 			}
 
 			
@@ -59,7 +65,7 @@ class  App extends React.Component {
 	render(){
 		return (
 			<div>
-				<Header currentUser = {this.state.currentUser}/>
+				<Header />
 				<Switch>
 					<Route exact={true} path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage}/>
@@ -71,7 +77,12 @@ class  App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+
+	setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null,mapDispatchToProps)(App);
 
 
   
